d944a988bb2ae594f0dc61e772a78b868ccfd5a4
RjDevOnrun <john.roby76@gmail.com>
2022-03-13 12:44:55 +0900

    Initial commit

---
+++ WpfApp/ViewModels/PersonCMViewModel.cs
@@ -0,0 +1,55 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace WpfApp.ViewModels
+{
+    /* 
+        This class uses the CommunityToolKit.MVVM Nuget Package (8.00 preview-2) 
+        
+        - Very easy WPF ViewModels, just using the Attribute Annotations
+        - Very clean codebase..
+        - all codes required to run are generated automatically
+
+        - For properties:
+            - annotate with [ObservableProperty]
+            - to notify other props use [AlsoNotifyChangeFor]
+            - to notify Command CanExecutes... use [AlsoNotifyCanExecuteFor]
+
+        - For Commands:
+            - Method to Execute: annotate with [ICommand]
+            - Method to CanExecute: annotate with [ICommand(CanExecute(nameof(MethodToCheck))]
+     */
+
+
+    [INotifyPropertyChanged]
+    public partial class PersonCMViewModel
+    {
+        [ObservableProperty]
+        [AlsoNotifyChangeFor(nameof(FullName))]
+        [AlsoNotifyCanExecuteFor(nameof(BuildFullNameCommand))]
+        string firstName = string.Empty;
+
+        [ObservableProperty]
+        [AlsoNotifyChangeFor(nameof(FullName))]
+        [AlsoNotifyCanExecuteFor(nameof(BuildFullNameCommand))]
+        string lastName = string.Empty;
+
+        [ObservableProperty]
+        string fullName = string.Empty;
+
+
+        [ICommand(CanExecute = nameof(CanBuildFullName))]
+        void BuildFullName()
+        {
+            FullName = FirstName + " " + LastName;
+        }
+
+        bool CanBuildFullName()
+        {
+            if((string.IsNullOrEmpty(firstName)) && (string.IsNullOrEmpty(lastName))) return false;
+
+            return true;
+        }
+
+    }
+}
---
+++ WpfApp/ViewModels/PersonMvxViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MvvmCross.Commands;
+using MvvmCross.ViewModels;
+using System.Windows.Input;
+
+namespace WpfApp.ViewModels
+{
+    /*
+     *  This class uses the MvvmCross Nuget Package 
+     *  
+     *  View model uses traditional MVVM pattern for WPF application
+     *  
+     */
+
+    public class PersonMvxViewModel: MvxNotifyPropertyChanged
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public string FullName { get; set; } = string.Empty;
+
+        ICommand nameCommand;
+        public ICommand NameCommand => nameCommand ??= new MvxCommand(BuildFullName);
+
+
+        void BuildFullName()
+        {
+            FullName = FirstName + " " + LastName;
+            RaisePropertyChanged(() => FullName);
+        }
+
+    }
+}
